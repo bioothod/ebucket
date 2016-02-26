33c4ea83e64012cb7e8b02630811c4d40773182d
Evgeniy Polyakov <zbr@ioremap.net>
2016-02-27 01:50:49 +0300

    bucket_processor: added initalization via bucket key - key which hosts list of EOL-separated buckets, this allows to update bucket list without bucket processor reinitialization, bucket key is being reread periodically and bucket list is automatically refreshed in the bucket processor

--- include/ebucket/bucket_processor.hpp
+++ include/ebucket/bucket_processor.hpp
@@ -42,7 +42,8 @@ public:
 	bucket_processor(std::shared_ptr<elliptics::node> &node) :
 	m_node(node),
 	m_stat(node),
-	m_error_session(*m_node)
+	m_error_session(*m_node),
+	m_buckets_update(std::bind(&bucket_processor::buckets_update, this))
 	{
 		m_error_session.set_exceptions_policy(elliptics::session::no_exceptions);
 		m_error_session.set_filter(elliptics::filters::all_with_ack);
@@ -55,10 +56,28 @@ public:
 			m_buckets_update.join();
 	}
 
+	bool init(const std::vector<int> &mgroups, const std::string &bucket_key) {
+		if (bucket_key.empty())
+			return false;
+
+		std::unique_lock<std::mutex> lock(m_lock);
+		m_bucket_key = bucket_key;
+		m_meta_groups = mgroups;
+		lock.unlock();
+
+		if (!m_bucket_key.empty()) {
+			auto err = request_bucket_list(m_bucket_key, true);
+			if (err)
+				return false;
+
+			init(mgroups, m_bnames);
+		}
+
+		return true;
+	}
+
 	bool init(const std::vector<int> &mgroups, const std::vector<std::string> &bnames) {
 		std::map<std::string, bucket> buckets = read_buckets(mgroups, bnames);
-
-		m_buckets_update =  std::thread(std::bind(&bucket_processor::buckets_update, this));
 
 		std::unique_lock<std::mutex> lock(m_lock);
 
@@ -160,6 +179,8 @@ public:
 			}
 		}
 
+		float eq_min = 0.9;
+		float eq_max = 1.1;
 		for (auto it = good_buckets.begin(), end = good_buckets.end(); it != end; ++it) {
 			float ratio = (float)it->counter/(float)num;
 			float wratio = it->weight / sum;
@@ -167,12 +188,16 @@ public:
 			// @ratio is a number of this bucket selection related to total number of runs
 			// it should rougly correspond to the ratio of free space in given bucket, or weight
 
-
-			BH_LOG(log, DNET_LOG_INFO, "test: bucket: %s, weight: %f, weight ratio: %f, selection ratio: %f",
-					it->b->name(), it->weight, wratio, ratio);
-
 			float eq = ratio / wratio;
-			if (eq > 1.2 || eq < 0.8) {
+
+			BH_LOG(log, DNET_LOG_INFO, "test: bucket: %s, weight: %f, counter: %d/%d, "
+					"weight ratio: %f, selection ratio: %f, ratio/wratio: %.2f (must be in [%.2f, %.2f]",
+					it->b->name(), it->weight,
+					it->counter, num,
+					wratio, ratio,
+					eq, eq_min, eq_max);
+
+			if (eq > eq_max || eq < eq_min) {
 				std::ostringstream ss;
 				ss << "bucket: " << it->b->name() <<
 					", weight: " << it->weight <<
@@ -287,16 +312,17 @@ private:
 	std::mutex m_lock;
 	std::vector<int> m_meta_groups;
 
+	std::string m_bucket_key;
 	std::vector<std::string> m_bnames;
 	std::map<std::string, bucket> m_buckets;
-
-	bool m_need_exit = false;
-	std::condition_variable m_wait;
-	std::thread m_buckets_update;
 
 	elliptics_stat m_stat;
 
 	elliptics::session m_error_session;
+
+	bool m_need_exit = false;
+	std::condition_variable m_wait;
+	std::thread m_buckets_update;
 
 	std::map<std::string, bucket> read_buckets(const std::vector<int> mgroups, const std::vector<std::string> &bnames) {
 		std::map<std::string, bucket> buckets;
@@ -328,8 +354,70 @@ private:
 		return buckets;
 	}
 
+	void received_bucket_list(const elliptics::sync_read_result &result, const elliptics::error_info &error) {
+		elliptics::logger &log = m_node->get_log();
+
+		if (error) {
+			BH_LOG(log, DNET_LOG_ERROR, "received_bucket_list: key: %s: could not read bucket list, error: %s [%d]",
+					m_bucket_key.c_str(), error.message().c_str(), error.code());
+			return;
+		}
+
+		const elliptics::data_pointer &file = result[0].file();
+
+		std::vector<std::string> bnames;
+
+		char *start = file.data<char>();
+		for (size_t i = 0; i < file.size(); ++i) {
+			char *n = file.data<char>() + i;
+			if (*n == '\n' || i == file.size() - 1) {
+				if (i == file.size() - 1) {
+					n++;
+				}
+
+				std::string bname(start, n - start);
+				bnames.emplace_back(bname);
+
+				start = n + 1;
+			}
+		}
+
+		std::lock_guard<std::mutex> guard(m_lock);
+		m_bnames = bnames;
+	}
+
+	elliptics::error_info request_bucket_list(const std::string &key, bool sync) {
+		elliptics::session s(*m_node);
+		s.set_exceptions_policy(elliptics::session::no_exceptions);
+		s.set_groups(m_meta_groups);
+
+		std::string ns = "bucket";
+		s.set_namespace(ns.c_str(), ns.size());
+
+		auto ret = s.read_data(key, 0, 0);
+		if (!ret.is_valid()) {
+			return elliptics::create_error(-EINVAL, "async read data is not valid");
+		}
+		if (sync) {
+			ret.wait();
+			if (ret.error())
+				return ret.error();
+
+			received_bucket_list(ret.get(), ret.error());
+		} else {
+			ret.connect(std::bind(&bucket_processor::received_bucket_list, this,
+					std::placeholders::_1, std::placeholders::_2));
+
+		}
+
+		return elliptics::error_info();
+	}
+
 	void buckets_update() {
 		while (!m_need_exit) {
+			if (!m_bucket_key.empty())
+				request_bucket_list(m_bucket_key, true);
+
 			std::unique_lock<std::mutex> guard(m_lock);
 			if (m_wait.wait_for(guard, std::chrono::seconds(30), [&] {return m_need_exit;}))
 				break;
